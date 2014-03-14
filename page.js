3fee4a99935f8160bd68ca3d9925afd802ee9a03
Nathan Laan <nathanlaan@gmail.com>
2014-03-14 11:13:32 -0600

    Basic image hiding working. Not working with Reddit RES.

---
+++ page.js
@@ -0,0 +1,19 @@
+// 
+// TextOnly - page.js
+//
+
+//
+// TODO: get enabled
+//
+
+	
+$("img").removeAttr("src").removeAttr("width").removeAttr("height");
+$(".thumbnail").removeAttr("width").removeAttr("height");
+
+//
+//
+//
+$(document).ready(function() {
+});
+
+
